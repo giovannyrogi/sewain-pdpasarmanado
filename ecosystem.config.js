8f71fa640437180ac5fd89bb0fce7d5ae65c2e98
giovannyrogi <giovannirogi@gmail.com>
2025-12-07 23:39:05 +0800

    add config for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "SewaIN",
+      script: "npm",
+      args: "start",
+      env: {
+        HOST: "127.0.0.1",
+        PORT: 3000
+      }
+    }
+  ]
+}
